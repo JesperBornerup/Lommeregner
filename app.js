63a5adc63a4e4660438f5067898df8047fad071a
KirstineElvira <kirstine.elvira@gmail.com>
2022-11-10 14:48:19 +0100

    sp

--- app.js
+++ app.js
@@ -87,10 +87,10 @@ var kampPoint = function(a, b, c){
     return 0;
 }
 var seriePoint = function(a, b, c){
-    if (a==VVV) {return 9}
-    if (a==VVU) {return 7}
-    if (a==VUU) {return 5}
-    if (a==UUU) {return 3}
+    if (a=="VVV") {return 9}
+    if (a=="VVU") {return 7}
+    if (a=="VUU") {return 5}
+    if (a=="UUU") {return 3}
 
 }
 var vinder =  function(a, b, c){
